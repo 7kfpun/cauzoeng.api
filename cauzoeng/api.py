98a422629c9f7e67424cdf1fc76b2454f0cc5be4
kf <7kfpun@gmail.com>
2014-04-02 23:41:58 +0800

    change name; use model only

--- cauzoeng/api.py
+++ cauzoeng/api.py
@@ -1,70 +1,23 @@
 # -*- coding: utf-8 -*-
 import endpoints
-from protorpc import messages
-from protorpc import message_types
 from protorpc import remote
-from models import Greeting, GreetingCollection, STORED_GREETINGS
-
-from google.appengine.ext import ndb
-from endpoints_proto_datastore.ndb import EndpointsModel
+from models import Lottery
 
 
-package = 'Hello'
+package = 'cauzoeng'
 
 
-class MyModel(EndpointsModel):
-    attr1 = ndb.StringProperty()
-    attr2 = ndb.StringProperty()
-    created = ndb.DateTimeProperty(auto_now_add=True)
+@endpoints.api(name='lottery', version='v1', description='Lottery API')
+class LotteryApi(remote.Service):
 
+    @Lottery.method(path='lottery', http_method='POST', name='lottery.insert')
+    def LotteryInsert(self, lottery):
+        lottery.put()
+        return lottery
 
-@endpoints.api(name='helloworld', version='v1', description='My Little API')
-class MyApi(remote.Service):
-
-    @MyModel.method(path='mymodel', http_method='POST', name='mymodel.insert')
-    def MyModelInsert(self, my_model):
-        my_model.put()
-        return my_model
-
-    @MyModel.query_method(path='mymodels', name='mymodel.list')
-    def MyModelList(self, query):
+    @Lottery.query_method(path='lottery', name='lottery.list')
+    def LotteryList(self, query):
         return query
 
 
-@endpoints.api(name='helloworld', version='v1')
-class HelloWorldApi(remote.Service):
-    """Helloworld API v1."""
-
-    @endpoints.method(message_types.VoidMessage, GreetingCollection,
-                      path='hellogreeting', http_method='GET',
-                      name='greetings.listGreeting')
-    def greetings_list(self, unused_request):
-        return STORED_GREETINGS
-
-    ID_RESOURCE = endpoints.ResourceContainer(
-        message_types.VoidMessage,
-        id=messages.IntegerField(1, variant=messages.Variant.INT32))
-
-    @endpoints.method(ID_RESOURCE, Greeting,
-                      path='hellogreeting/{id}', http_method='GET',
-                      name='greetings.getGreeting')
-    def greeting_get(self, request):
-        try:
-            return STORED_GREETINGS.items[request.id]
-        except (IndexError, TypeError):
-            raise endpoints.NotFoundException('Greeting %s not found.' %
-                                              (request.id,))
-
-    MULTIPLY_METHOD_RESOURCE = endpoints.ResourceContainer(
-        Greeting,
-        times=messages.IntegerField(2, variant=messages.Variant.INT32,
-                                    required=True))
-
-    @endpoints.method(MULTIPLY_METHOD_RESOURCE, Greeting,
-                      path='hellogreeting/{times}', http_method='POST',
-                      name='greetings.multiply')
-    def greetings_multiply(self, request):
-        return Greeting(message=request.message * request.times)
-
-
-APPLICATION = endpoints.api_server([MyApi])
+APPLICATION = endpoints.api_server([LotteryApi])
--- cauzoeng/models.py
+++ cauzoeng/models.py
@@ -1,74 +1,14 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime, timedelta
-from protorpc import messages
-from protorpc.message_types import DateTimeField
+from endpoints_proto_datastore.ndb import EndpointsModel
+from google.appengine.ext import ndb
 
 
-class Greeting(messages.Message):
-    """Greeting that stores a message."""
+class Lottery(EndpointsModel):
+    """ Lottery models."""
 
-    id = messages.IntegerField(1)
-    message = messages.StringField(2)
-    description = messages.StringField(3)
-    subject = messages.StringField(4, default='MAMA')
-    url = messages.StringField(5)
-    created_date = DateTimeField(6)
-    finish_date = DateTimeField(7)
-
-
-class GreetingCollection(messages.Message):
-    """Collection of Greetings."""
-    items = messages.MessageField(Greeting, 1, repeated=True)
-
-
-STORED_GREETINGS = GreetingCollection(items=[
-    Greeting(
-        id=0, message='hello world!',
-        subject="lucky draw 1",
-        description="this is the 1st lucky draw!",
-        url='https://developers.google.com/appengine/docs/python/tools/protorpc/messages/fieldclass',  # noqa
-        finish_date=datetime.now() + timedelta(days=14),
-    ),
-    Greeting(
-        id=1, message='goodbye world!',
-        subject="lucky draw 2",
-        description="this is the 2nd lucky draw!",
-        url='https://developers.google.com/appengine/docs/python/tools/uploadinganapp',  # noqa
-        finish_date=datetime.now() + timedelta(days=24),
-    ),
-    Greeting(
-        id=2, message='goodbye again world!',
-        subject="lucky draw 3",
-        description="this is the 3rd lucky draw!",
-        url='https://developers.google.com/appengine/docs/python/',
-        finish_date=datetime.now() + timedelta(days=34),
-    ),
-    Greeting(
-        id=3, message='goodbye again world!',
-        subject="lucky draw 4",
-        description="this is the 3rd lucky draw!",
-        url='https://developers.google.com/appengine/docs/python/',
-        finish_date=datetime.now() + timedelta(days=34),
-    ),
-    Greeting(
-        id=4, message='goodbye again world!',
-        subject="lucky draw 5",
-        description="this is the 3rd lucky draw!",
-        url='https://developers.google.com/appengine/docs/python/',
-        finish_date=datetime.now() + timedelta(days=34),
-    ),
-    Greeting(
-        id=5, message='goodbye again world!',
-        subject="lucky draw 6",
-        description="this is the 3rd lucky draw!",
-        url='https://developers.google.com/appengine/docs/python/',
-        finish_date=datetime.now() + timedelta(days=34),
-    ),
-    Greeting(
-        id=6, message='goodbye again world!',
-        subject="lucky draw 7",
-        description="this is the 3rd lucky draw!",
-        url='https://developers.google.com/appengine/docs/python/',
-        finish_date=datetime.now() + timedelta(days=34),
-    ),
-])
+    title = ndb.StringProperty()
+    subject = ndb.StringProperty()
+    description = ndb.StringProperty()
+    url = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    finished = ndb.DateTimeProperty(auto_now=True)
